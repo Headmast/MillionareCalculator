4d78436d0030698b687d32bec8628c8014e8eb5a
Kirill Klebanov <headmast@bk.ru>
2015-10-27 00:06:56 +0300

    Add class file for GameObject: Sector and Company.

---
+++ MillionareCalculator/GameObject/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillionareCalculator.GameObject
+{
+    class Company
+    {
+
+        private int price;
+        private int tax;
+        private int rent;
+        private int buldingPrice;
+
+    }
+}
