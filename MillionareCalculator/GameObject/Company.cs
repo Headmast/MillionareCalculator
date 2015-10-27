410b0ef27a36955afdda00a898666c923548e223
Kirill Klebanov <headmast@bk.ru>
2015-10-27 20:23:38 +0300

    Add properties in Company class. Add price, tax, rent, building price, count buildings, is monopoly buildings.

--- MillionareCalculator/GameObject/Company.cs
+++ MillionareCalculator/GameObject/Company.cs
@@ -9,10 +9,63 @@ namespace MillionareCalculator.GameObject
     class Company
     {
 
-        private int price;
-        private int tax;
-        private int rent;
-        private int buldingPrice;
+        private int _Price;
 
+        public int Price
+        {
+            get { return _Price; }
+            set { _Price = value; }
+        }
+
+        private int _Tax;
+
+        public int Tax
+        {
+            get { return _Tax; }
+            set { _Tax = value; }
+        }
+
+        private int _Rent;
+
+        public int Rent
+        {
+            get { return _Rent; }
+            set { _Rent = value; }
+        }
+
+        private int _BuildingPrice;
+
+        public int BuldingPrice
+        {
+            get { return _BuildingPrice; }
+            set { _BuildingPrice = value; }
+        }
+
+        private int _CountBuildings;
+
+        public int CountBuldings
+        {
+            get { return _CountBuildings; }
+            set { _CountBuildings = value; }
+        }
+
+        private Boolean _IsMonopolyBuilding;
+
+        public Boolean IsMonopolyBuilding
+        {
+            get { return _IsMonopolyBuilding; }
+            set { _IsMonopolyBuilding = value; }
+        }
+
+
+        public int GetAllRent ()
+        {
+            return this.Rent;
+        }
+
+        public int GetAllTax ()
+        {
+            return this.Tax;
+        }
     }
 }
